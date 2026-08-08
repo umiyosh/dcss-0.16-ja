19dff96ce397266cec4b8cd2513311d18d885d58
umiyosh <yumino99@gmail.com>
2026-08-09 04:51:45 +0900

    feat(i18n): translate spell descriptions

--- crawl-ref/source/test/description-db-ja.lua
+++ crawl-ref/source/test/description-db-ja.lua
@@ -1,6 +1,12 @@
 local cases = {
   { "rat", "不潔な齧歯類" },
   { "Abjuration spell", "元いた場所に送還" },
+  { "Controlled Blink spell", "正確に制御可能な短距離" },
+  { "Glaciate spell", "猛烈な氷の奔流" },
+  { "Dragon's Call spell", "竜の領域" },
+  { "Necromutation spell", "力ある骸骨" },
+  { "Drain Magic spell", "反魔法の武器" },
+  { "Tornado spell", "力強い旋風" },
   { "club", "重量がある木の棒" },
   { "A closed door", "木製の扉" },
 }
